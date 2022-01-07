cec2e1b190c54e445a41c555283465b37dec95e8
dnaldoog <zan642@gmail.com>
2022-01-07 10:45:34 +0900

    Calculator Code

--- Gemwest/GCalc.cpp
+++ Gemwest/GCalc.cpp
@@ -1,2 +1,136 @@
 #include "pch.h"
 #include "GCalc.h"
+String^ GCalc::gem_round_formula(void) {
+    //double calculate;
+    //String^ sg = returnSG();
+    //String^ depth = returnDepth();
+    //double avd = returnAvdiameter().todouble();
+    //String^ fac = returnFactor();
+    //sum_all_adjustments();
+    //String^ adj = returnAdj();
+    //calculate = (avd * avd) * depth.todouble() * sg.todouble() * adj.todouble() * fac.todouble();
+    //int x = returnDecPlaces(calculate);
+    //return String ^ ::number(calculate, 'f', x);
+    String^ dummy = "";
+    return dummy;
+}
+String^ GCalc::gem_round_bead_formula(void) {
+    //double calculate;
+    //String^ length = returnLength();
+    //double avd = length.todouble();
+    //String^ sg = returnSG();
+    //String^ fac = returnFactor();
+    //double avdcubed = avd * avd * avd;
+    //sum_all_adjustments();
+    //String^ adj = returnAdj();
+    //calculate = (avdcubed)*sg.todouble() * adj.todouble() * fac.todouble();
+
+    //int x = returnDecPlaces(calculate);
+    //return String ^ ::number(calculate, 'f', x);
+    String^ dummy = "";
+    return dummy;
+}
+String^ GCalc::gem_lwd_formula(void) {
+    //double calculate;
+    //String^ length = returnLength();
+    //String^ width = returnWidth();
+    //String^ sg = returnSG();
+    //String^ depth = returnDepth();
+    //String^ fac = returnFactor();
+    //sum_all_adjustments();
+    //String^ adj = returnAdj();
+    //calculate = (length.todouble() * width.todouble()) * depth.todouble() * sg.todouble() * adj.todouble() * fac.todouble();
+    //int x = returnDecPlaces(calculate);
+    //return String ^ ::number(calculate, 'f', x);
+    String^ dummy = "";
+    return dummy;
+}
+String^ GCalc::gem_oval_formula(void) {
+    //double calculate;
+    //String^ length = returnLength();
+    //String^ width = returnWidth();
+    //String^ sg = returnSG();
+    //String^ depth = returnDepth();
+    //String^ fac = returnFactor();
+    //sum_all_adjustments();
+    //String^ adj = returnAdj();
+    //double oval_av = (length.todouble() + width.todouble()) / 2;
+    //calculate = (oval_av * oval_av) * depth.todouble() * sg.todouble() * adj.todouble() * fac.todouble();
+    //int x = returnDecPlaces(calculate);
+    //return String ^ ::number(calculate, 'f', x);
+    String^ dummy = "";
+    return dummy;
+}
+String^ GCalc::gem_briolette_formula(void) {
+    //double calculate;
+    //String^ length = returnLength();
+    //String^ width = returnWidth();
+    //String^ sg = returnSG();
+    ///*String ^ depth=returnDepth();*/
+    //String^ fac = returnFactor();
+    //sum_all_adjustments();
+    //String^ adj = returnAdj();
+    //calculate = (length.todouble() * width.todouble()) * width.todouble() * sg.todouble() * adj.todouble() * fac.todouble();
+    //int x = returnDecPlaces(calculate);
+    //return String ^ ::number(calculate, 'f', x);
+    String^ dummy = "";
+    return dummy;
+}
+String^ GCalc::returnWeight(void) {
+    //String^ answer, cut_table = "gem_cuts";
+    //String^ c = returnCut();
+    //int index = 0;
+    //QSqlDatabase db = QSqlDatabase::database("MyDB");
+    //if (db.open()) {
+    //}
+    //else {
+    //}
+    //QSqlQueryModel* model = new QSqlQueryModel();
+    //String^ sql;
+    //sql = "SELECT cut,factor,xpm,dia FROM `" + cut_table + "` WHERE `cut`='" + c + "'";
+    //QSqlQuery* query = new QSqlQuery(db);
+    //query->prepare(sql);
+    //model->setQuery(*query);
+    //if (!query->exec()) {
+    //}
+    //else {
+    //    while (query->next())
+    //    {
+
+    //        index = query->value(3).toInt();
+    //    }
+    String^ dummy = "";
+    return dummy;
+    }
+
+void GCalc::Initializer(
+    String^ cutName,
+    String^ fac,
+    String^ d1,
+    String^ d2,
+    String^ dep,
+    String^ sg,
+    String^ gt,
+    String^ pv,
+    String^ adj,
+    bool isdiamond,
+    bool interp,
+    bool recut,
+    bool roundish,
+    bool depthisperc) {
+    // load all arrays of fancy cut adjustments
+    /*load class with GUI values from user*/
+    _cutName = cutName; // name of cut
+    _factor = fac; // name of cut
+    _d1 = d1; // diameter 1 (could be length)
+    _d2 = d2; // diamter 2 (could be width)
+    _depth = dep; // depth of stone in millimeters // or percentage
+    _sg = sg; //Specific Gravity
+    _isDiamond = isdiamond;
+    _adj = adj; // user adjustments  
+    _interp = interp; // interpolate?
+    _recut = recut; // recut?
+    _roundish = roundish; // is the cut round necessitating in a different LW formula?
+    _depthIsPerc = depthisperc; // is the incoming Depth a represenatation of percentage or mm?
+
+}
--- Gemwest/GCalc.h
+++ Gemwest/GCalc.h
@@ -4,5 +4,58 @@ using namespace System::Collections::Generic;
 
 ref class GCalc
 {
+private:
+    //Dictionary<double, double>^ _marquise_formulae;
+    //Dictionary<double, double>^ _emerald_formulae;
+    //Dictionary<double, double>^ _pear_formulae;
+    //Dictionary<String^, String^>^ _em_map;
+    //Dictionary<String^, String^>^ _mq_map;
+    //Dictionary<String^, String^>^ _pe_map;
+    //void dictInitializer();
+    /*following are Form1 data passed to object*/
+    String^ _cutName; // name of cut
+    String^ _factor; // factor selected by cut choice
+    String^ _d1; // diameter 1 (could be length)
+    String^ _d2; // diamter 2 (could be width)
+    String^ _depth; // depth of stone in millimeters
+    String^ _sg; //Specific Gravity
+    String^ _gt; // girdle thickness
+    String^ _pv; // pavilion bulge
+    String^ _adj; // user adjustments  
+    bool _isDiamond; // is it a diamond (not as found in the gem listing)
+    bool _interp; // interpolate?
+    bool _recut; // recut?
+    bool _roundish; // is the cut round necessitating in a different LW formula?
+    bool _depthIsPerc; // is the incoming Depth a represenatation of percentage or mm?
+
+
+
+String^ gem_round_formula(void);
+String^ gem_round_bead_formula(void);
+String^ gem_lwd_formula(void);
+String^ gem_oval_formula(void);
+String^ gem_briolette_formula(void);
+String^ returnWeight(void);
+
+public:
+    void Initializer(
+        String^ cutName,
+        String^ fac,
+        String^ d1,
+        String^ d2,
+        String^ dep,
+        String^ sg,
+        String^ gt,
+        String^ pv,
+        String^ adj,
+        bool isdiamond,
+        bool interp,
+        bool recut,
+        bool roundish,
+        bool depthisperc
+    );
+
+    /*end constructor*/
+    String^ calculate();
 };
 
